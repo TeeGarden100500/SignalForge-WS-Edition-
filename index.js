d33cd494d5d3c3a1e05076216277fe5cef45331d
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-10 21:13:31 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const { initVolatilityWatcher } = require('./core/volatilitySelector');
 const { initWebSocket } = require('./websocket/wsHandler');
+const { initVolatilityWatcher } = require('./core/volatilitySelector');
+const { startWSManager } = require('./ws/smartWSManager');
+
+
 
 initVolatilityWatcher();
 initWebSocket();
+initVolatilityWatcher();
+startWSManager();
