171fbbe29d07c84b0f11c1ccd48122b8b5b10d23
TeeGarden100500 <hexus1984ab@gmail.com>
2025-05-13 02:33:26 +0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,17 +5,17 @@ module.exports = {
 
   // ░░░ RSI ░░░
   RSI_PERIOD: 60,
-  RSI_LOW: 40,
-  RSI_HIGH: 60,
+  RSI_LOW: 30,
+  RSI_HIGH: 70,
 
   // ░░░ EMA Crossover ░░░
-  EMA_FAST: 8,
-  EMA_SLOW: 21,
+  EMA_FAST: 12,
+  EMA_SLOW: 26,
 
   // ░░░ MACD ░░░
-  MACD_FAST: 6,
-  MACD_SLOW: 13,
-  MACD_SIGNAL: 4,
+  MACD_FAST: 12,
+  MACD_SLOW: 26,
+  MACD_SIGNAL: 9,
 
   // ░░░ Mean Reversion ░░░
   MEAN_REVERSION_MA_PERIOD: 30,
@@ -23,22 +23,22 @@ module.exports = {
 
   // ░░░ ATR ░░░
   ATR_PERIOD: 14,
-  MIN_ATR_PERCENT: 0.1,
+  MIN_ATR_PERCENT: 0.5,
 
   // ░░░ ADX ░░░
   ADX_PERIOD: 14,
-  MIN_ADX: 5,
+  MIN_ADX: 20,
 
   // ░░░ Объем ░░░
-  VOLUME_LOOKBACK: 5,
-  VOLUME_SPIKE_MULTIPLIER: 1.2,
+  VOLUME_LOOKBACK: 20,
+  VOLUME_SPIKE_MULTIPLIER: 2.5,
 
   // ░░░ Пробой ░░░
   BREAKOUT_LOOKBACK: 60,
   BREAKOUT_MARGIN_PERCENT: 0.05,
 
   // ░░░ Фильтрация сигналов ░░░
-  SIGNAL_CONFIRMATION_COUNT: 1,
+  SIGNAL_CONFIRMATION_COUNT: 2,
   SIGNAL_TIME_WINDOW_UTC: {
     start: '00:00',
     end: '23:59'
@@ -50,8 +50,8 @@ module.exports = {
 
   // ░░░ Логика и кэш ░░░
   MAX_CACHE_LENGTH: 500,
-  DEBUG_LOGGING: true,
-  DEBUG_LOG_LEVEL: 'verbose', // none | basic | verbose
+  DEBUG_LOGGING: false,
+  DEBUG_LOG_LEVEL: 'none', // none | basic | verbose
 
   // ░░░ EMA Impulse Angle ░░░
   EMA_ANGLE_PERIOD: 3,
@@ -59,7 +59,7 @@ module.exports = {
   EMA_ANGLE_LENGTH: 5,
 
   // ░░░ Волатильность ░░░
-  VOLATILITY_TOP_COUNT: 10,
+  VOLATILITY_TOP_COUNT: 30,
   VOLATILITY_LOOKBACK: 30,
   VOLATILITY_REFRESH_INTERVAL_SEC: 3600
 };
